83c8f80899c7ebf6f59f5c7c81d9b57d2b7c26b8
PretzelOptician <alekschultz1@gmail.com>
2022-12-17 02:21:16 -0500

    almost getting game logs to work

--- probitModelData.py
+++ probitModelData.py
@@ -67,10 +67,88 @@ def convert_team_name(team):
     elif team == 'NewOrleans' or team == 'New Orleans': return 'New Orleans Pelicans'
     elif team == 'GoldenState' or team == 'Golden State': return 'Golden State Warriors'
     elif team == 'LAClippers' or team == 'LA Clippers': return 'Los Angeles Clippers'
-    elif team == 'Charlotte': return 'Los Angeles Clippers'
+    elif team == 'Charlotte': return 'Charlotte Hornets'
     elif team == 'Dallas': return 'Dallas Mavericks'
 
+def get_abbrv(team): 
+    if team == 'LALakers' or team == 'LA Lakers': return 'LAL'
+    elif team == 'Cleveland': return 'CLE'
+    elif team == 'Boston': return 'BOS'
+    elif team == 'Milwaukee': return 'MIL'
+    elif team == 'Chicago': return 'CHI'
+    elif team == 'Portland': return 'POR'
+    elif team == 'Toronto': return 'TOR'
+    elif team == 'Philadelphia': return 'PHI'
+    elif team == 'Atlanta': return 'ATL'
+    elif team == 'Orlando': return 'ORL'
+    elif team == 'Brooklyn': return 'BRK'
+    elif team == 'Washington': return 'WAS'
+    elif team == 'Miami': return 'MIA'
+    elif team == 'NewYork': return 'NYK'
+    elif team == 'Indiana': return 'IND'
+    elif team == 'Detroit': return 'DET'
+    elif team == 'OklahomaCity' or team == 'Oklahoma City': return 'OKC'
+    elif team == 'Sacramento': return 'SAC'
+    elif team == 'Minnesota': return 'MIN'
+    elif team == 'Phoenix': return 'PHO'
+    elif team == 'SanAntonio' or team == 'San Antonio': return 'SAS'
+    elif team == 'Memphis': return 'MEM'
+    elif team == 'Denver': return 'DEN'
+    elif team == 'Houston': return 'HOU'
+    elif team == 'Utah': return 'UTA'
+    elif team == 'NewOrleans' or team == 'New Orleans': return 'NOP'
+    elif team == 'GoldenState' or team == 'Golden State': return 'GSW'
+    elif team == 'LAClippers' or team == 'LA Clippers': return 'LAC'
+    elif team == 'Charlotte': return 'CHA'
+    elif team == 'Dallas': return 'DAL'
+
+def get_game_log(team, year): 
+    team_str = get_abbrv(team)
+    year_str = str(year)
+
+    api_url = f"https://www.basketball-reference.com/teams/{team_str}/{year_str}/gamelog/?sr&amp;utm_source=direct&amp;utm_medium=Share&amp;utm_campaign=ShareTool#tgl_basic"
+    response = requests.get(api_url) 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table')
+    leagueData = pd.read_html(str(table))[0]
+    leagueData.columns = leagueData.columns.droplevel(level=0)
+
+    ## ChatGPT code: filtering data set
+    # Convert the 'G' column to a numeric type and store in a new column 'G_num'
+    leagueData['G_num'] = pd.to_numeric(leagueData['G'], errors='coerce')
+
+    # Filter the rows where 'G_num' is not null
+    df = leagueData[leagueData['G_num'].notnull()]
+
+    df = df.reset_index(drop=True)
+    return df
+
+pd.set_option('display.max_rows', None)
 total_data = pd.DataFrame({'year': [], 'hitOver': [], 'total': [], 'avg_popularity': [], 'totalppg': [], 'size_of_spread': [], 'home_team': [], 'away_team': [], 'pct_overs_hit': [], 'avg_prev_year_efg_pct_off': [], 'avg_prev_year_tov_pct_off': [], 'avg_prev_year_orb_pct': [], 'avg_prev_year_ft_per_fga_off': [], 'avg_prev_year_efg_pct_def': [], 'avg_prev_year_tov_pct_def': [], 'avg_prev_year_drb_pct': [], 'avg_prev_year_ft_per_fga_def': [], 'avg_prev_year_netrtg': [], 'avg_prev_year_pace': []})
+
+team_names = [ "Atlanta", "Boston", "Brooklyn", "Charlotte", "Chicago", "Cleveland", "Dallas", "Denver", "Detroit", "Golden State", "Houston", "Indiana", "LA Clippers", "LA Lakers", "Memphis", "Miami", "Milwaukee", "Minnesota", "New Orleans", "New York", "Oklahoma City", "Orlando", "Philadelphia", "Phoenix", "Portland", "Sacramento", "San Antonio", "Toronto", "Utah", "Washington"]
+
+team_name_mapping = {
+    "LALakers": "LA Lakers",
+    "GoldenState": "Golden State",
+    "LAClippers": "LA Clippers", 
+    "NewOrleans": "New Orleans", 
+    "SanAntonio": "San Antonio", 
+    "OklahomaCity": "Oklahoma City", 
+    "NewYork": "New York"
+}
+
+# Initialize an empty dictionary to store the dataframes
+game_logs = {}
+
+# Iterate over the list of teams and the range of years
+for team in team_names:
+    for year in range(2014, 2022):
+        # Use the get_game_log function to retrieve the dataframe for the current team and year
+        df = get_game_log(team, year)
+        # Add the dataframe to the dictionary with the key (team, year)
+        game_logs[(team, year)] = df
+        print("Generated game log for " + team + " in " + str(year) + "...")
 
 for yearOffset in range(9):
     year = 2014+yearOffset
@@ -86,7 +164,9 @@ for yearOffset in range(9):
     new_data = pd.DataFrame({'year': [], 'hitOver': [], 'total': [], 'avg_popularity': [], 'totalppg': [], 'size_of_spread': [], 'home_team': [], 'away_team': [], 'pct_overs_hit': [], 'avg_prev_year_efg_pct_off': [], 'avg_prev_year_tov_pct_off': [], 'avg_prev_year_orb_pct': [], 'avg_prev_year_ft_per_fga_off': [], 'avg_prev_year_efg_pct_def': [], 'avg_prev_year_tov_pct_def': [], 'avg_prev_year_drb_pct': [], 'avg_prev_year_ft_per_fga_def': [], 'avg_prev_year_netrtg': [], 'avg_prev_year_pace': []})
     for row in range(data.shape[0]): 
         if row%2==1: 
-            print('for: ' + data.at[row, 'Team'] + ' vs ' + data.at[row-1, 'Team'])
+            team1 = data.at[row, 'Team']
+            team2 = data.at[row-1, 'Team']
+            print('for: ' + team1 + ' vs ' + team2)
             if data.at[row, 'Close']!='pk' and data.at[row, 'Close']>=100:
                 #is the total
                 total = data.at[row, 'Close']
@@ -106,19 +186,19 @@ for yearOffset in range(9):
             homeppg = 0
             gamesPlayed = 0
             for row2 in range(row-1): 
-                if data.at[row2, 'Team'] == data.at[row, 'Team']: 
+                if data.at[row2, 'Team'] == team1: 
                     homeppg+= data.at[row2, 'Final']
                     gamesPlayed += 1
             homeppg /= (gamesPlayed if gamesPlayed>0 else 1)
             awayppg = 0
             gamesPlayed = 0
             for row2 in range(row-1): 
-                if data.at[row2, 'Team'] == data.at[row-1, 'Team']: 
+                if data.at[row2, 'Team'] == team2: 
                     awayppg+= data.at[row2, 'Final']
                     gamesPlayed += 1
             awayppg /= (gamesPlayed if gamesPlayed>0 else 1)
             totalppg = homeppg + awayppg
-            avg_followers = (get_avg_followers(data.at[row-1, 'Team']) + get_avg_followers(data.at[row, 'Team']))/2
+            avg_followers = (get_avg_followers(team2) + get_avg_followers(team1))/2
             
             # four factors stuff
             o_efg = 0
@@ -132,7 +212,7 @@ for yearOffset in range(9):
             netrating = 0
             pace = 0
             for row2 in range(leagueData.shape[0]): 
-                if leagueData.iat[row2, 0].startswith(convert_team_name(data.at[row, 'Team'])) or leagueData.iat[row2, 0].startswith(convert_team_name(data.at[row-1, 'Team'])): 
+                if leagueData.iat[row2, 0].startswith(convert_team_name(team1)) or leagueData.iat[row2, 0].startswith(convert_team_name(team2)): 
                     o_efg += leagueData.iat[row2, 17]
                     o_tov += leagueData.iat[row2, 18]
                     orb += leagueData.iat[row2, 19]
@@ -154,6 +234,11 @@ for yearOffset in range(9):
             netrating /= 2
             pace /= 2
             
+
+            ## calculate pace
+            game_log_1 = game_logs[(team_name_mapping.get(team1, team1), year)]
+            game_log_2 = game_logs[(team_name_mapping.get(team2, team2), year)]
+
             if not push: 
                 new_data.loc[len(new_data.index)] = [year, (1 if hitOver else 0), total, avg_followers, totalppg, size_of_spread, data.at[row, 'Team'], data.at[row-1, 'Team'], None, o_efg, o_tov, orb, o_ftfga, d_efg, d_tov, drb, d_ftfga, netrating, pace]
 
--- test.py
+++ test.py
@@ -2,12 +2,111 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-api_url = "https://widgets.sports-reference.com/wg.fcgi?css=1&site=bbr&url=%2Fleagues%2FNBA_" + str(2014) + ".html&div=div_advanced-team"
-response = requests.get(api_url) 
-soup = BeautifulSoup(response.content, 'html.parser')
-table = soup.find('table')
-leagueData = pd.read_html(str(table))[0]
-leagueData = leagueData.drop(['Unnamed: 0_level_0'], axis=1)
-for row2 in range(leagueData.shape[0]): 
-    if leagueData.iat[row2, 0].startswith("San Antonio Spurs") or leagueData.iat[row2, 0].startswith("San Antonio Spurs"): 
-        print(leagueData.iat[row2, 21])
+# year = 2022
+# api_url = "https://www.basketball-reference.com/teams/BRK/2022/gamelog/?sr&amp;utm_source=direct&amp;utm_medium=Share&amp;utm_campaign=ShareTool#tgl_basic"
+# response = requests.get(api_url) 
+# soup = BeautifulSoup(response.content, 'html.parser')
+# table = soup.find('table')
+# leagueData = pd.read_html(str(table))[0]
+# leagueData.columns = leagueData.columns.droplevel(level=0)
+# # print(leagueData)
+# # Convert the 'G' column to a numeric type and store in a new column 'G_num'
+# leagueData['G_num'] = pd.to_numeric(leagueData['G'], errors='coerce')
+
+# # Filter the rows where 'G_num' is not null
+# df = leagueData[leagueData['G_num'].notnull()]
+# df = df.reset_index(drop=True)
+# pd.set_option('display.max_rows', None)
+# print(df)
+
+def get_abbrv(team, year): 
+    if team == 'LALakers' or team == 'LA Lakers': return 'LAL'
+    elif team == 'Cleveland': return 'CLE'
+    elif team == 'Boston': return 'BOS'
+    elif team == 'Milwaukee': return 'MIL'
+    elif team == 'Chicago': return 'CHI'
+    elif team == 'Portland': return 'POR'
+    elif team == 'Toronto': return 'TOR'
+    elif team == 'Philadelphia': return 'PHI'
+    elif team == 'Atlanta': return 'ATL'
+    elif team == 'Orlando': return 'ORL'
+    elif team == 'Brooklyn': return 'BRK'
+    elif team == 'Washington': return 'WAS'
+    elif team == 'Miami': return 'MIA'
+    elif team == 'NewYork': return 'NYK'
+    elif team == 'Indiana': return 'IND'
+    elif team == 'Detroit': return 'DET'
+    elif team == 'OklahomaCity' or team == 'Oklahoma City': return 'OKC'
+    elif team == 'Sacramento': return 'SAC'
+    elif team == 'Minnesota': return 'MIN'
+    elif team == 'Phoenix': return 'PHO'
+    elif team == 'SanAntonio' or team == 'San Antonio': return 'SAS'
+    elif team == 'Memphis': return 'MEM'
+    elif team == 'Denver': return 'DEN'
+    elif team == 'Houston': return 'HOU'
+    elif team == 'Utah': return 'UTA'
+    elif team == 'NewOrleans' or team == 'New Orleans': return 'NOP'
+    elif team == 'GoldenState' or team == 'Golden State': return 'GSW'
+    elif team == 'LAClippers' or team == 'LA Clippers': return 'LAC'
+    elif team == 'Charlotte': 
+        if year < 2015: return 'CHA'
+        else: return 'CHO'
+    elif team == 'Dallas': return 'DAL'
+
+def get_game_log(team, year): 
+    team_str = get_abbrv(team, year)
+    year_str = str(year)
+    print(team_str)
+    print(year_str)
+
+    api_url = f"https://www.basketball-reference.com/teams/{team_str}/{year_str}/gamelog-advanced/?sr&utm_source=direct&utm_medium=Share&utm_campaign=ShareTool#tgl_advanced"
+    print(api_url)
+    response = requests.get(api_url) 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table')
+    leagueData = pd.read_html(str(table))[0]
+    leagueData.columns = leagueData.columns.droplevel(level=0)
+
+    ## ChatGPT code: filtering data set
+    # Convert the 'G' column to a numeric type and store in a new column 'G_num'
+    leagueData['G_num'] = pd.to_numeric(leagueData['G'], errors='coerce')
+
+    # Filter the rows where 'G_num' is not null
+    df = leagueData[leagueData['G_num'].notnull()]
+
+    df = df.reset_index(drop=True)
+    return df
+
+team_names = [ "Atlanta", "Boston", "Brooklyn", "Charlotte", "Chicago", "Cleveland", "Dallas", "Denver", "Detroit", "Golden State", "Houston", "Indiana", "LA Clippers", "LA Lakers", "Memphis", "Miami", "Milwaukee", "Minnesota", "New Orleans", "New York", "Oklahoma City", "Orlando", "Philadelphia", "Phoenix", "Portland", "Sacramento", "San Antonio", "Toronto", "Utah", "Washington"]
+
+team_name_mapping = {
+    "LALakers": "LA Lakers",
+    "GoldenState": "Golden State",
+    "LAClippers": "LA Clippers", 
+    "NewOrleans": "New Orleans", 
+    "SanAntonio": "San Antonio", 
+    "OklahomaCity": "Oklahoma City", 
+    "NewYork": "New York"
+}
+
+# Initialize an empty dictionary to store the dataframes
+game_logs = {}
+
+print(get_game_log("Cleveland", 2017))
+
+# Iterate over the list of teams and the range of years
+for team in team_names:
+    for year in range(2014, 2022):
+        # Use the get_game_log function to retrieve the dataframe for the current team and year
+        df = get_game_log(team, year)
+        # Add the dataframe to the dictionary with the key (team, year)
+        game_logs[(team, year)] = df
+        print("Generated game log for " + team + " in " + str(year) + "...")
+
+team1 = "New York"
+print(game_logs[(team_name_mapping.get(team1, team1), year)])
+team1 = "SanAntonio"
+print(game_logs[(team_name_mapping.get(team1, team1), year)])
+team1 = "Minnesota"
+print(game_logs[(team_name_mapping.get(team1, team1), year)])
+
